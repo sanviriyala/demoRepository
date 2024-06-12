b010acd003b3ba6a3b2c99933173ad045c2c3762
vjagansa <viriyala-naga-venkata.jagan-sai-santhosh@capgemini.com>
2024-06-12 16:18:01 +0530

    changes in branch

--- src/SampleClass.java
+++ src/SampleClass.java
@@ -2,5 +2,6 @@ public SampleClass {
     public void sampleMethod() {
         System.out.println("Github demo project");
         System.out.println("This is a code change for codeBranch");
+        System.out.println("This is a code change for codeBranch from cli");
     }
 }
